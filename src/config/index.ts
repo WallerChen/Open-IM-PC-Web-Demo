e6e36e16915474d3024282ef2a5aac0739e84367
chenweirui <chenweirui@MacBook-Pro.local>
2023-08-16 23:26:11 +0800

    update ws

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // WS 10001 API 10002 CHAT 10008 CONFIG 10009
 
-export const WS_URL = "ws://chatapi.teamaiservices.com/websocket";
+export const WS_URL = "wss://chatapi.teamaiservices.com/websocket";
 export const API_URL = "https://chatapi.teamaiservices.com/api";
 export const USER_URL = "https://chatapi.teamaiservices.com/user";
